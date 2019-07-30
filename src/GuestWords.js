04e14fe2533b3595bcab621abe3d58f5a8955fdd
Benjamin Perez <benjaminperez@Benjamins-MacBook-Pro.local>
2019-07-30 17:15:24 -0500

    Added guessed words component

---
+++ src/GuestWords.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuestWords = (props) => {
+    let content = '';
+    
+    if(props.guessedWords.length === 0){
+        content = (
+            <span data-test="guess-instructions">
+                Try to guess the secret word!
+            </span>
+        );
+    }
+
+    return(
+        <div data-test="component-guessed-words">
+            {content}
+        </div>
+    );
+};
+
+GuestWords.propTypes = {
+    guessedWords: PropTypes.arrayOf(
+        PropTypes.shape({
+            guessedWord: PropTypes.string.isRequired,
+            letterMatchCount: PropTypes.number.isRequired,
+        }),
+    ).isRequired,
+};
+
+export default GuestWords;
