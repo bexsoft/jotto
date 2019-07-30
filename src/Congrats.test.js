cb7655d9ccc33dd9b4df6934bc7dbc6a41bb941a
Benjamin Perez <benjaminperez@Benjamins-MacBook-Pro.local>
2019-07-30 15:48:57 -0500

    Enzyme config

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import Congrats from './Congrats';
 import { findByTest, checkProps } from '../test/testUtils';
-
-Enzyme.configure({adapter: new EnzymeAdapter()});
 
 const setup = (props = {}) => {
     return shallow(<Congrats {...props} />);
